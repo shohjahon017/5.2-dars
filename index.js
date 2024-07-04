5227689e2b39fddec374e8735861359b37cd0087
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-07-04 10:46:02 +0500

    .

---
+++ index.js
@@ -0,0 +1,46 @@
+//3-qism. Masalalar
+// 1-masala
+// let arr = {
+//   name: "Aziz",
+//   age: 24,
+//   job: "builder",
+// };
+// function objectkeys(arr) {
+//   return arr;
+// }
+// console.log(objectkeys(Object.keys(arr)));
+
+//2-masala
+// let arr = {
+//   name: "Asadbek",
+//   age: 17,
+//   phone: "+998931236547",
+// };
+// function objectvalues(arr) {
+//   return arr;
+// }
+// console.log(objectvalues(Object.values(arr)));
+
+//3-masala
+// let arr = {
+//   name: "Islom",
+//   age: 12,
+//   phone: "+998939856547",
+// };
+// function objectvalues(arr) {
+//   return arr;
+// }
+// console.log(objectvalues(Object.entries(arr)));
+
+//4-masala
+// let arr = {
+//   name: "Komil",
+//   age: 37,
+//   email: "komil@gmail.com",
+// };
+// function objectvalues(arr) {
+//   return arr;
+// }
+// console.log(objectvalues(Object.freeze(arr)));
+// arr.age = 25;
+// console.log(arr);
